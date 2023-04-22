27a8751ad136cfd20c94895c668eddb5ee290303
Dale <66420874+Dale-Elder@users.noreply.github.com>
2023-04-22 17:36:09 +0100

    pushing completed tasks

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,25 @@
-const http = require("http");
-const port = 5000;
+const express = require("express");
+const path = require("path");
+// create express app
+const app = express();
+// setting up the port if not set by the environment
+const port = 5000 || process.env.port;
+// setting up the static files and middleware
+app.use(express.static("./public"));
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Welcome to our home page</h1>");
-  res.end();
+// about page
+// app.get("/", (req, res) => {
+//   res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assets
+// SSR
+// });
+
+// 404 page
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found!</h1>");
 });
 
-server.listen({ port }, () => {
-  console.log(`server is listening on port ${port}`);
+// app listening at localhost on port 5000 or the environment port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- 02-express-tutorial/final/05-all-static.js
+++ 02-express-tutorial/final/05-all-static.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+// create express app
+const app = express();
+// setting up the port if not set by the environment
+const port = 5000 || process.env.port;
+// setting up the static files and middleware
+app.use(express.static("./public"));
 
-const app = express()
+// about page
+// app.get("/", (req, res) => {
+//   res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assets
+// SSR
+// });
 
-// setup static and middleware
-app.use(express.static('./public'))
+// 404 page
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found!</h1>");
+});
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-//   adding to static assets
-//   SSR
-// })
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
-
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+// app listening at localhost on port 5000 or the environment port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
