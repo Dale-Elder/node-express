213c0ce9fea6ec9be6e74d5f7db2caf905941009
Dale <66420874+Dale-Elder@users.noreply.github.com>
2023-04-23 19:41:35 +0100

    refactored port .env code line

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,7 +4,7 @@ const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 
 /*-- middleware --*/
