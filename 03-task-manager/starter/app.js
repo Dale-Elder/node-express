4e143c804a1d8f4f48d6cb0941d0914f583da877
Dale <66420874+Dale-Elder@users.noreply.github.com>
2023-04-23 08:36:43 +0100

    working on tutorials

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = 3000 || process.env.PORT;
+
+// routes
+app.get("/hello", (req, res) => {
+  res.status(200).send("Task Manager App!");
+});
+
+// app.get('/api/v1/tasks')        -- get all the tasks
+// app.post('/api/v1/tasks')       -- create a new task
+// app.get('/api/v1/tasks/:id')    -- get a single task
+// app.patch('/api/v1/tasks/:id')  -- update a task
+// app.delete('/api/v1/tasks/:id') -- delete a task
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running at http://localhost:${port}...`);
+});
